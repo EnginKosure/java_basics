725054a2d8028bb8cbcef5ba5c2f3118a14f7c8b
EnginKosure <engin.kosure@cronos.be>
2021-03-12 23:29:38 +0100

    write first class

---
+++ src/Hw.java
@@ -0,0 +1,12 @@
+public class Hw {
+    // main: entry point to the program
+    public static void main(String[] args) {
+        System.out.println("New language, new adventure!");
+    }
+}
+// Java code must be compiled before run
+//  Source code => compiles to Byte code
+// javac Hw.java   --> run that command to compile -->Hw.class file will be generated
+// Now we can run our program
+// Write the name of the class without the extension
+// java Hw
