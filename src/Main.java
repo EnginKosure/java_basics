7a02e03723bf02b92990a5e51664e400dc4c13fa
EnginKosure <engin.kosure@cronos.be>
2021-03-14 10:30:09 +0100

    explore string

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("IntelliJ setup");
+        int studentAge=15;
+        double studentGPA=3.54;
+        char studentLastInitial='H';
+        boolean hasPerfectAttendance=true;
+        String studentName= "Someone Somebody";
+        char studentFirstInitial=studentName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(studentName);
+        System.out.println(studentFirstInitial);
+
     }
 }
