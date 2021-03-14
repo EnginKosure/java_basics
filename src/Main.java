f63db044bcb2d24112513ecb37ded9429e80b9ff
EnginKosure <engin.kosure@cronos.be>
2021-03-15 00:24:00 +0100

    explore control flow

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,29 @@ public class Main {
 
         System.out.println(studentFirstName + " " + studentLastName +
                 " now has a GPA of " + studentGPA);
+
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner=new Scanner(System.in);
+
+        int inputtedNum=scanner.nextInt();
+
+        if (inputtedNum<5){
+            System.out.println("Enjoy the good luck");
+        } else{
+            System.out.println("Always good luck");
+        }
+
+        boolean isOnRepeat=true;
+        while(isOnRepeat){
+            System.out.println("Playing current song");
+            System.out.println("Turn of repeat? If so, answer yes");
+            String userInput=input.next();
+
+            if(userInput.equals("yes")){
+                isOnRepeat=false;
+            }
+        }
+        System.out.println("Playing next");
     }
 }
 
