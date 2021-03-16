7fee8ca76608cbe8e78b311f980ca7bffa8d05b1
EnginKosure <engin.kosure@cronos.be>
2021-03-16 23:31:21 +0100

    add return type

--- src/Functions.java
+++ src/Functions.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class Functions {
 
-    public static void calcTotalPrice(double listedMealPrice, double tipRate, double taxRate){
+    public static double calcTotalPrice(double listedMealPrice, double tipRate, double taxRate){
         double tip=tipRate*listedMealPrice;
         double tax=taxRate*listedMealPrice;
         double result=listedMealPrice+tip+tax;
-        System.out.println("Your total meal price is "+result);
+        return result;
     }
 
     public static void main(String[] args) {
@@ -17,9 +17,3 @@ public class Functions {
 
     }
 }
-// Java code must be compiled before run
-//  Source code => compiles to Byte code
-// javac Hw.java   --> run that command to compile -->Hw.class file will be generated
-// Now we can run our program
-// Write the name of the class without the extension
-// java Hw
