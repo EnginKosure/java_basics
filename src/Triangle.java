cf36a507f0de249156d35115fdc11c373f2ca457
EnginKosure <engin.kosure@cronos.be>
2021-03-21 23:58:01 +0100

    add comments

--- src/Triangle.java
+++ src/Triangle.java
@@ -14,7 +14,8 @@ public class Triangle {
         this.sideLenTwo=sideLenTwo;
         this.sideLenThree=sideLenThree;
     }
-
+// This is an instance method, we must call it over an instance.
+    // If it was a static method, we would call it directly over the Class (like Math.pow())
     public double findArea(){
         return this.base*this.height/2;
     }
