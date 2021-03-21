5844e1abdd60a98221aad85b05aa2f7a258266a2
EnginKosure <engin.kosure@cronos.be>
2021-03-21 23:22:20 +0100

    Add Triangle class

---
+++ src/Triangle.java
@@ -0,0 +1,18 @@
+public class Triangle {
+//    instance variables
+    double base;
+    double height;
+    double sideLenOne;
+    double sideLenTwo;
+    double sideLenThree;
+
+//    create constructor
+    public Triangle(double base, double height, double sideLenOne, double sideLenTwo, double sideLenThree){
+        this.base=base;
+        this.height=height;
+        this.sideLenOne=sideLenOne;
+        this.sideLenTwo=sideLenTwo;
+        this.sideLenThree=sideLenThree;
+    }
+
+}
